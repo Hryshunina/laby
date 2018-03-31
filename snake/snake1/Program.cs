09ce2e2940fda152c15cf4b91549d9f4b753ae6a
Marina <marina.grishunina98@gmail.com>
2018-03-31 15:20:53 +0300

    Hello world

--- snake/snake1/Program.cs
+++ snake/snake1/Program.cs
@@ -10,25 +10,8 @@ namespace snake1
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
-
-            Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym) // процедурный подход. Функция вывода точек на экран
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            Console.WriteLine("Hello world");
+            Console.ReadLine(); // запрос нажатия Enter
         }
     }
 }

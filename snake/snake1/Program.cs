7789ef9705ad35799b73d6fd98d02032a67bb300
Marina <marina.grishunina98@gmail.com>
2018-04-01 13:57:19 +0300

    Вертикальная линия, рамка

--- snake/snake1/Program.cs
+++ snake/snake1/Program.cs
@@ -10,20 +10,29 @@ namespace snake1
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*'); // обращение к классу Point (точка) - экземмпляр класс
-            p1.Draw(); // рисуем точку
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#'); 
-            p2.Draw();
+            // формируем рамку
+            HorisontalLine upline = new HorisontalLine(0, 78, 0, '+');
+            upline.Draw();
 
-            HorisontalLine line = new HorisontalLine(5, 10, 8, '+');
-            line.Draw();
+            HorisontalLine downline = new HorisontalLine(0, 78, 24, '+');
+            downline.Draw();
 
-            VerticalLine line2 = new VerticalLine(7, 11, 8, '-');
-            line2.Draw();
+            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            leftline.Draw();
 
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            rightline.Draw();
+             
+            // рисуем точку
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
             Console.ReadLine();
+            
         }
     }
 }

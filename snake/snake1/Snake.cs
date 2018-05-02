4354582983b692fc7adf4b012420c38f2f998be5
Hryshunina <marina.grishunina98@gmail.com>
2018-05-02 23:12:30 +0300

    Препатствия для змейки - рамка

--- snake/snake1/Snake.cs
+++ snake/snake1/Snake.cs
@@ -57,6 +57,17 @@ namespace snake1
                 return false;
         }
 
+        public bool IsHitTail()
+        {
+            var head = pList.Last();
+            for (int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                    return true;
+            }
+            return false;
+        }
+
         public void HandleKey(ConsoleKey key)
         {
             if (key == ConsoleKey.LeftArrow)
